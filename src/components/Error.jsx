0258e8846ecee16af8d046a35ed44e4ee60d49ad
Alexis Martinez <as.alexismartinez@gmail.com>
2022-12-12 22:26:41 -0300

    finalizando proyecto

---
+++ src/components/Error.jsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled'
+
+const Texto = styled.div`
+    background-color: #b7322c;
+    color: #fff;
+    padding: 15px;
+    font-size: 22px;
+    text-transform: uppercase;
+    font-family: 'lato', sans-serif;
+    font-weight: bold;
+    text-align: center;
+`
+
+
+const Error = ({children}) => {
+    return (
+        <Texto>
+            {children}
+        </Texto>
+    )
+}
+
+export default Error
